0b9e33f3d600f7e605124f00fe89407dfd9cf54b
Yash Malviya <ymalviya@oneshield.com>
2026-02-10 22:09:59 +0530

    init

---
+++ fenwick_tree/simple.cpp
@@ -0,0 +1,24 @@
+struct FenwickTree {
+  vector<long long> fenw;
+  int n;
+
+  FenwickTree(int n_) : n(n_) {
+    fenw.resize(n);
+  }
+  
+  void modify(int i, int v) {
+    while (i <= n) {
+      fenw[i] += v;
+      i += i & -i;
+    }
+  }
+
+  long long query(int i) {
+    long long sum = 0;
+    while (i > 0) {
+      sum += fenw[i];
+      i -= i & -i;
+    }
+    return sum;
+  }
+};
